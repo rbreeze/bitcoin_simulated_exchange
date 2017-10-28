9a7aba43fba18d4396591f57871de8db4efd27fa
rbreeze <remington.breeze@gmail.com>
2017-10-28 11:01:30 -0700

    Set up basic Express hello world server running on port 3000 and added dependecies to packages.json

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log('Hello world!');
+const express = require('express');
+const app = express(); 
+
+app.listen(process.env.PORT || 3000);
+console.log('Server is running on port 3000');
+
+// Use the public directory to host frontend files.
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+  res.send("Hello world!");
+})
