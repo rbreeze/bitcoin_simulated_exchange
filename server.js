3a9a374f98528c72b935b8f2ffa6a473a3c5e6ac
rbreeze <remington.breeze@gmail.com>
2017-10-28 11:50:48 -0700

    Added Handlebars templating engine and Bootstrap CSS styling.

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
+// Include dependencies.
 const express = require('express');
+const expressHandlebars = require('express-handlebars');
 const app = express(); 
 
+// Run the app on port 3000. 
 app.listen(process.env.PORT || 3000);
 console.log('Server is running on port 3000');
+
+// Set the templating engine to Handlebars.
+app.engine('handlebars', expressHandlebars({ defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 // Use the public directory to host frontend files.
 app.use(express.static(__dirname + '/public'));
 
+// Use the views directory to host Handlebar views.
+app.set('/views', __dirname + '/views');
+
+// Serve the home page.
 app.get('/', function(req, res) {
-  res.send("Hello world!");
-})
+  res.render('index', { title: "Hello!" });
+});
