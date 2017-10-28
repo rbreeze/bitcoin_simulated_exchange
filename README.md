7fea40429510ecd4e864229d5b319a2a0ada4777
rbreeze <remington.breeze@gmail.com>
2017-10-28 13:47:33 -0700

    Added sign up and log in functionality, and defined working User, Transaction and Asset schemas.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# bitcoin_simulated_exchange
+# BSX - Bitcoin Simulated Exchange
+
 Simulation game for trading cryptocurrencies. Uses a Node.js server and MongoDB to host users and call a cryptocurrency exchange rate API.
--- server.js
+++ server.js
@@ -1,11 +1,54 @@
 // Include dependencies.
 const express = require('express');
+const bodyParser = require('body-parser');
 const expressHandlebars = require('express-handlebars');
+const passwordHash = require('password-hash');
+const mongoose = require('mongoose');
 const app = express(); 
+var session = require('express-session');
 
 // Run the app on port 3000. 
 app.listen(process.env.PORT || 3000);
 console.log('Server is running on port 3000');
+
+// Connect to local mongodb instance. 
+mongoose.connect('mongodb://localhost:27017');
+
+// Define User and Asset schemas and models for Mongoose.
+var transactionSchema = new mongoose.Schema({
+  name : {type : String, default: 'Bitcoin'},
+  tag : {type : String, default: 'BTC'},
+  price: {type : Number, default: 0},
+  quantity: {type : Number, default: 0},
+  buy: {type : Boolean, default: true}
+});
+
+var assetSchema = new mongoose.Schema({
+  name : {type : String, default: 'Bitcoin'},
+  tag : {type : String, default: 'BTC'},
+  price: {type : Number, default: 0},
+  quantity: {type : Number, default: 0}
+});
+
+var userSchema = new mongoose.Schema({
+  username: {type : String, default: ''},
+  password: {type : String, default: ''},
+  transactions: {type : [Transaction], default: ''},
+  assets: {type : [Asset], default: ''},
+  liquidAssets: {type: Number, default: '10000'},
+  netWorth: {type: Number, default: '10000'}
+});
+
+var Transaction = mongoose.model('Transaction', transactionSchema);
+var Transaction = mongoose.model('Asset', assetSchema);
+var User = mongoose.model('User', userSchema);
+
+// Configure the session variable.
+app.use(session({secret: 'zQWNr3Pj_!!842CDN'}));
+
+// Set up body parsing for form input.
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Set the templating engine to Handlebars.
 app.engine('handlebars', expressHandlebars({ defaultLayout: 'main'}));
@@ -19,5 +62,52 @@ app.set('/views', __dirname + '/views');
 
 // Serve the home page.
 app.get('/', function(req, res) {
-  res.render('index', { title: "Hello!" });
+  res.render('index', { title: "Home" });
+});
+
+// Serve the login page.
+app.get('/login', function(req, res) {
+  if (req.session.user && req.session.auth)
+    res.redirect("/user/" + req.session.user._id);
+  else
+    res.render("login", { title: "Log In"});
+});
+
+// Serve the signup page.
+app.get('/signup', function(req, res) {
+  res.render('signup', { title: "Sign Up" });
+})
+
+// Handle a login request.
+app.post('/login', function(req, res) {
+  User.findOne({ 'username' : req.body.username }, function(err, user) {
+    if (!user)
+      res.render("login", { error: "No user with that username exists!" });
+    else if (err) 
+      res.render("login", { error: err });
+    else if (passwordHash.verify(req.body.password, user.password)) {
+      req.session.user = user;
+      req.session.auth = true;
+      res.render("user", { user : user });
+    } else {
+      res.render("login", { error: "Incorrect password." });
+    }
+  });
+});
+
+// Handle a signup request.
+app.post('/signup', function(req, res) {
+  var user = new User(req.body);
+  user.password = passwordHash.generate(req.body.password);
+  User.findOne({ "username" : req.body.username }, function(err, data) {
+    if (data)
+      res.render("signup", { title: "Sign Up", error : "A user with that username address already exists!" });
+    else {
+      user.save(function(err, data) {
+        if (err)
+          res.send(err);
+        res.render("login", { title: "Successful Sign Up", message: "Please log in with your new credentials." });
+      });
+    }
+  });
 });
